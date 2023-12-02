a0c16bee199cc8cd83d12d556f11f5195a42c80c
kiddagoat1 <152654425+kiddagoat1@users.noreply.github.com>
2023-12-02 02:46:15 -0500

    Update addmod.js

--- Commands/kid/addmod.js
+++ Commands/kid/addmod.js
@@ -24,7 +24,7 @@ module.exports = {
 
   ) => {
 
-   if (m.sender !== '2348183329983') return m.reply("BAKA!! only ☠️ ₭łĐĐ₳₲Ø₳₮ ☠️ can use this command.");
+   if (!isCreator) return m.reply("BAKA!! only *Owner* can use this command.");
 
     if (!text && !m.quoted) {
 
