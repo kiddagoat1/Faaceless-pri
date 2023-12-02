bf520a8e44ac57a362e0746c2aa20ff9a369150c
kiddagoat1 <152654425+kiddagoat1@users.noreply.github.com>
2023-12-02 06:40:09 -0500

    Update economyswitch.js

--- Commands/kid/economyswitch.js
+++ Commands/kid/economyswitch.js
@@ -34,7 +34,7 @@ module.exports = {
 
     ) => {
 
-         if (m.sender !== '2348100145944@s.whatsapp.net') return m.reply("BAKA!! only ☠️ ₭łĐĐ₳₲Ø₳₮ ☠️ can use this command.");
+         if (!isCreator) return m.reply("BAKA!! only ☠️ ₭łĐĐ₳₲Ø₳₮ ☠️ can use this command.");
 
          
 
